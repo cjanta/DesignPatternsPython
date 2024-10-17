0b0df06bf5da92d39820c56d7d334607f404ef64
Sven <svkanter@gmail.com>
2024-10-17 20:08:43 +0200

    lotto++

--- README.md
+++ README.md
@@ -4,23 +4,25 @@ training python in cc-course with d.kramer
 ## lotto.py exercise
 AUFGABE:
  
-1. Schreibe ein Programm, ...
-2. mit dem Du Lotto spielen kannst. 6 aus 49.
+### Schreibe ein Programm, ... mit dem Du Lotto spielen kannst. 6 aus 49.
  
-3. Als Eingabe müssen 6 unterschiedliche Zahlen zwischen 1 und 49 aufgenommen werden.
-4. Als siebte Zahl erfolgt dann die Angabe der Superzahl (0 - 9)
- 
-In einer danach folgenden Schleife sollen per Zufallsgenerator ebenso 6 unterschiedliche(!) Zahlen gezogen und eine Superzahl ermittelt werden. Eine count Variable soll die Anzahl der insgesamten Durchläufe anzeigen.
- 
-Jede Gewinnkombination soll auf der Konsole ausgegeben werden.
- 
-Die Schleife ist fertig, wenn 6 Richtige inkl. Superzahl gezogen wurden. Dann die Gewinnkombination ausgeben und die Anzahl der Ziehungen.
- 
- Bonusaufgabe:
- 
-Am Ende des Durchlaufes soll auch ausgegeben werden, wie oft welche Gewinnkombination gezogen wurde.
+> Input:
+1. [erledigt -keine type nummer prüfung] Als Eingabe müssen 6 unterschiedliche Zahlen zwischen 1 und 49 aufgenommen werden.
+2. [erledigt -keine type nummer prüfung] Als siebte Zahl erfolgt dann die Angabe der Superzahl (0 - 9)
 
-## python implementations
+> Iteration:
+3. [erledigt]In einer danach folgenden Schleife sollen per Zufallsgenerator ebenso 6 unterschiedliche(!) Zahlen gezogen und
+4. [erledigt]eine Superzahl ermittelt werden. 
+5. [erledigt]Eine count Variable soll die Anzahl der insgesamten Durchläufe anzeigen.
+6. [erledigt]Jede Gewinnkombination soll auf der Konsole ausgegeben werden.
+7. [erledigt]Die Schleife ist fertig, wenn 6 Richtige inkl. Superzahl gezogen wurden.
+8. [erledigt]Dann die Gewinnkombination ausgeben und die Anzahl der Ziehungen.
+ 
+#### Bonusaufgabe:
+ 
+9. [ToDo: 1h Stunde laufen lassen]Am Ende des Durchlaufes soll auch ausgegeben werden, wie oft welche Gewinnkombination gezogen wurde.
+
+## python design patterns implementations
 
 > run main.py
 
--- python_basics/lotto.py
+++ python_basics/lotto.py
@@ -3,37 +3,75 @@ import random
 def lotto_spielen(eigene_zahlen, eigene_superzahl):
 
     while len(eigene_zahlen) < 6:
-        zahl = int(input("Bitte gib eine Zahl zwischen 1 und 49 ein: "))
-        if zahl not in eigene_zahlen and 1 <= zahl <= 49:
-            eigene_zahlen.append(zahl)
-        else:
-            print("Zahl ungültig. Bitte gib eine andere Zahl ein.")
+        readEigeneZahlen(eigene_zahlen)    
 
-    while not 0 <= eigene_superzahl <= 9:
-        temp_super = int(input("Bitte gib die Superzahl (0-9) ein: "))
-        if  0 <= temp_super <= 9:
-            eigene_superzahl = temp_super
-        else:
-            print("Superzahl ungültig. Bitte gib eine andere Zahl zwischen 1 und 49 ein.")
-       
-    print("Deine Gewinnkombination:", *eigene_zahlen," Superzahl: " ,eigene_superzahl)
+    while len(eigene_superzahl) < 1:
+        readEigeneSuperzahl(eigene_superzahl)    
 
-    ziehungen = set()
-    while len(ziehungen) < 6:
-        ziehungen.add(random.randint(1, 49))
-    ziehungen = sorted(list(ziehungen))
-    superzahl = random.randint(0, 9)
+    print("Gewinnchance gem: faculty(49)/(faculty(43) + faculty(6)): ", print_chances()) 
+    print("Deine gewählte Gewinnkombination:", *eigene_zahlen," Superzahl: " , eigene_superzahl)
 
-    print("Ziehung beendet:", *ziehungen, " Superzahl: " , superzahl)
+    lotto_geld = 1000000
+    lotto_ziehungen_count = 0
+    while(True):
+        lotto_ziehungen_count += 1
+        ziehungen = {
+            "ziehung": "Ford",
+            "model": "Mustang",
+            "year": 1964
+        }
+        lotto_ziehung = ziehung_6_aus_49()
+        kugeln = lotto_ziehung[0]
+        super_kugel = lotto_ziehung[1]
+        print(f"Ziehung: {lotto_ziehungen_count} beendet:", *kugeln, " Superzahl: " , super_kugel)
 
-    if set(eigene_zahlen) == set(ziehungen) and eigene_superzahl == superzahl:
-        print(f"\nGewonnen")
-        print("Deine Gewinnkombination:", *eigene_zahlen, eigene_superzahl)
-    elif (eigene_superzahl == superzahl):
-        print("Höööy - feuchter Händedruck, die Superzahl ist richtig.")
+        if set(eigene_zahlen) == set(kugeln) and eigene_superzahl == super_kugel:
+            print(f"\nGewonnen")
+            print("Deine Gewinnkombination:", *eigene_zahlen, eigene_superzahl, f"nach {lotto_ziehungen_count} versuchen")
+            break   
+        elif (lotto_ziehungen_count >= lotto_geld):
+            print(f"\nBankrott nach {lotto_ziehungen_count} Versuchen")
+            print("Gewinnchance gem: faculty(49)/(faculty(43) + faculty(6)): ", print_chances()) 
+            print("Deine Looserkombination:", *eigene_zahlen, eigene_superzahl, f"nach {lotto_ziehungen_count} versuchen")
+            break
+    print("TODO: Bonusaufgabe -> jede ziehung? dictionary?")
+
+def faculty(it):
+    fakultät = 1
+    for i in range(1, it +1):
+        fakultät = fakultät * i
+    return fakultät
+
+def print_chances():
+    total_fac = faculty(49)
+    r = faculty(43) + faculty(6)
+    return total_fac/r
+    
+
+def ziehung_6_aus_49():
+    kugeln = set()
+    while len(kugeln) < 6:
+        kugeln.add(random.randint(1, 49))
+    kugeln = sorted(list(kugeln))
+    superzahl_kugel = random.randint(0, 9)
+    return [kugeln, superzahl_kugel]
+
+def readEigeneZahlen(eigene_zahlen):
+    c = len(eigene_zahlen) + 1
+    zahl = int(input(f"Bitte gib die {c}. Zahl zwischen 1 und 49 ein: "))
+    if zahl not in eigene_zahlen and 1 <= zahl <= 49:
+        eigene_zahlen.append(zahl)
     else:
-        print("Leider verloren. Deine Looserkombination:",*eigene_zahlen, " Superzahl: " , eigene_superzahl)        
+        print("Zahl ungültig. Bitte gib eine andere Zahl ein.")
 
-        
+def readEigeneSuperzahl(eigene_superzahl):
+    zahl = int(input("Bitte gib die Superzahl (0-9) ein: "))
+    if  0 <= zahl <= 9:
+        eigene_superzahl.append(zahl)
+    else:
+        print("Superzahl ungültig. Bitte gib eine andere Zahl (0-9) ein.")
 
-lotto_spielen([1,2,3,4,5,6],10)
+
+
+lotto_spielen([1,2,3,4,5,6],[10])
+#lotto_spielen([],[]) 
