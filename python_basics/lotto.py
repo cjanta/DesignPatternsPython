3df13b13acb0a2f29b0ce372c2611f969fb326ad
Sven <svkanter@gmail.com>
2024-10-17 17:50:45 +0200

    added lotto

---
+++ python_basics/lotto.py
@@ -0,0 +1,37 @@
+import random
+
+def lotto_spielen(eigene_zahlen, eigene_superzahl):
+
+    while len(eigene_zahlen) < 6:
+        zahl = int(input("Bitte gib eine Zahl zwischen 1 und 49 ein: "))
+        if zahl not in eigene_zahlen and 1 <= zahl <= 49:
+            eigene_zahlen.append(zahl)
+        else:
+            print("Zahl ungültig. Bitte gib eine andere Zahl ein.")
+
+    while not 1 <= eigene_superzahl <= 49:
+        temp_super = int(input("Bitte gib die Superzahl (0-9) ein: "))
+        if temp_super not in eigene_zahlen and 1 <= temp_super <= 49:
+            eigene_superzahl = temp_super
+        else:
+            print("Superzahl ungültig. Bitte gib eine andere Zahl zwischen 1 und 49 ein.")
+       
+    print("Deine Gewinnkombination:", *eigene_zahlen," Superzahl: " ,eigene_superzahl)
+
+    ziehungen = set()
+    while len(ziehungen) < 6:
+        ziehungen.add(random.randint(1, 49))
+    ziehungen = sorted(list(ziehungen))
+    superzahl = random.randint(0, 9)
+
+    print("Ziehung beendet:", *ziehungen, " Superzahl: " , superzahl)
+
+    if set(eigene_zahlen) == set(ziehungen) and eigene_superzahl == superzahl:
+        print(f"\nGewonnen")
+        print("Deine Gewinnkombination:", *eigene_zahlen, eigene_superzahl)
+    else:
+        print("Leider verloren. Deine Looserkombination:",*eigene_zahlen, " Superzahl: " , eigene_superzahl)        
+
+        
+
+lotto_spielen([1,2,3,4,5,6],10)
